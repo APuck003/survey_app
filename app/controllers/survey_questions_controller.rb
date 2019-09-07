3f71e0ba21f4c0fc93e839c53d77b98b3f8f0351
Austin <Austin_MacBook@austins-mbp.attlocal.net>
2019-09-07 16:28:36 -0500

    updated forms and removed fields_for

--- app/controllers/survey_questions_controller.rb
+++ app/controllers/survey_questions_controller.rb
@@ -8,26 +8,20 @@ class SurveyQuestionsController < ApplicationController
     @question = @survey.questions.new
   end
   
-  def show
-    @survey = Survey.find(params[:survey_id])
-    @question = Question.find_by_id(params[:question_id])
-    render :index
-  end
-
+  # def show
+  #   @survey = Survey.find(params[:survey_id])
+  #   @question = Question.find_by_id(params[:question_id])
+  #   render :index
+  # end
+  
   def create
-    @survey = Survey.find(params[:survey_id])
-    @question = @survey.question.new
-    redirect_to @survey
-    # if @question.save
-    #   redirect_to
-    #   # redirect_to survey_survey_questions_url(@survey)
-    # else
-    #   render :new
-    # end
-    
+    @question.survey_id = params[:survey_id]
+    @question.save
   end
   
   def update
+    # @question.survey_id = params[:survey_id]
+    # @question.update
     @survey = Survey.find(params[:survey_id])
     @question = @survey.questions.find_by_id(params[:question_id])
     if @question.update(survey_question_params)
@@ -35,10 +29,9 @@ class SurveyQuestionsController < ApplicationController
     else
       render 'edit'
     end
-    # @question = @survey.questions.update(survey_question_params)
-    
+  
   end
-
+  
   def edit
     @survey = Survey.find(params[:survey_id])
     @question = @survey.questions.find_by_id(params[:question_id])
@@ -50,9 +43,9 @@ class SurveyQuestionsController < ApplicationController
     @question.destroy
     redirect_to survey_path(@survey)
   end
-
+  
   private
-
+  
   def survey_question_params
     params.require(:question).permit(:question_id, :survey_id, :question)
   end
